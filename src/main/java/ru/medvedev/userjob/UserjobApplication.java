c00e8812b702c81f9466042274daba13389c2217
Вячеслав <medvedevvyavit@yandex.ru>
2023-06-24 11:42:59 +0300

    Initial commit

---
+++ src/main/java/ru/medvedev/userjob/UserjobApplication.java
@@ -0,0 +1,13 @@
+package ru.medvedev.userjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/medvedev/userjob/UserjobApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.medvedev.userjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
